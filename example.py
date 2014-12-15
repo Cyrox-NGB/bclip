fd4cb2c94ae92859cc597d8401d667db0ca49b68
Cyrox-NGB <cyrox@live.de>
2014-12-15 15:41:02 +0100

    Create example.py

---
+++ example.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from berryclip import BerryClip
+
+bc = BerryClip()
+bc.leds([1, 3, 5], True)
+bc.ledsPulse([2, 4, 6], 1000, True)
+bc.switchWait()
+bc.beeps(5, 3, 500, 500)
+bc.switchWait()
+bc.clean()
